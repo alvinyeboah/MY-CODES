f6a856d8cb86673c81c0c6c19c250629692f5374
arhinkofi022 <arhinkofi022@gmail.com>
2023-07-15 01:32:25 +0000

    first commit

---
+++ # this is me trying my second code.py
@@ -0,0 +1,8 @@
+# this is me trying my second code 
+number = float(input("Type your number"))
+if number < 0:
+     print("The number is negative")
+elif number == 0: 
+    print("This is Zero")
+else:
+     print("The number is positive")
---
+++ KOFI TRIAL KODE 1.py
@@ -0,0 +1,15 @@
+
+#print("Hello world")
+exam_score = int(input("What is your mark:"))
+if exam_score >= 90:
+    print("GPA 4.0")
+elif exam_score >= 80:
+    print("GPA 3.75")
+elif exam_score >= 70:
+    print("GPA 3.0")
+elif exam_score >= 60:
+    print("GPA 2.0")
+elif exam_score >= 50:
+        print("CONGRATS you passed")
+else:
+    print("loser")
