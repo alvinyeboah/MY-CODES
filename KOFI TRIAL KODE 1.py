88f01f7bfdc9c993456189731a9c7958e5160c66
alvinyeboah <alvinyeboah5@gmail.com>
2023-07-15 01:42:30 +0000

    Changes

--- KOFI TRIAL KODE 1.py
+++ KOFI TRIAL KODE 1.py
@@ -1,15 +1,34 @@
 
-#print("Hello world")
-exam_score = int(input("What is your mark:"))
-if exam_score >= 90:
-    print("GPA 4.0")
-elif exam_score >= 80:
-    print("GPA 3.75")
-elif exam_score >= 70:
-    print("GPA 3.0")
-elif exam_score >= 60:
-    print("GPA 2.0")
-elif exam_score >= 50:
-        print("CONGRATS you passed")
-else:
-    print("loser")
+# #print("Hello world")
+# exam_score = int(input("What is your mark:"))
+# if exam_score >= 90:
+#     print("GPA 4.0")
+# elif exam_score >= 80:
+#     print("GPA 3.75")
+# elif exam_score >= 70:
+#     print("GPA 3.0")
+# elif exam_score >= 60:
+#     print("GPA 2.0")
+# elif exam_score >= 50:
+#         print("CONGRATS you passed")
+# else:
+#     print("loser")
+
+
+
+def score_calculator(exam_score):
+    if exam_score >= 90:
+        return "GPA 4.0"
+    elif exam_score >= 80:
+        return "GPA 3.75"
+    elif exam_score >= 70:
+        return "GPA 3.0"
+    elif exam_score >= 60:
+        return "GPA 2.0"
+    elif exam_score >= 50:
+        return "CONGRATS you passed"
+    else:
+        return "loser"
+
+instance1 = score_calculator(90)
+print(instance1)
